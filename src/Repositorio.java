3a89ffcb2cd8b2b4660ca25c6b3a893a38beee17
Miguel Serrano <miguel.serrano.lopez@alumnos.upm.es>
2025-10-05 22:39:36 +0200

    Initial commit

---
+++ src/Repositorio.java
@@ -0,0 +1,13 @@
+
+public class Repositorio {
+
+	
+	static String a = "Buenos dias,";
+	static String b = " esta es mi primera entrega de GitHub,";
+	static String c = " muchas gracias";
+	
+	public static void main(String[] args) {
+		System.out.println(a + b + c);
+	}
+
+}
